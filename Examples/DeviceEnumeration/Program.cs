b7f2586eec9368bb3d2588b7f5f774425c2e6896
Victoria Beauray Sagady <cephypi@pm.me>
2025-11-11 08:00:49 -0600

    fix(examples): Refactored to display enumerated devices
    - Now enumerates devices and prints a report in console.
    - Added dependency UsbVendorsLibrary for name lookups.

--- Examples/DeviceEnumeration/Program.cs
+++ Examples/DeviceEnumeration/Program.cs
@@ -1,43 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics.CodeAnalysis;
 using GameInputDotNet;
 using GameInputDotNet.Interop.Enums;
+using UsbVendorsLibrary;
 
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
 internal class Program
 {
+    private const string ThickHr = "=========================================";
+    private const string ThinHr = "-----------------------------------------";
+
+    // Config For Test
+    private static readonly GameInputKind inputKind = GameInputKind.Controller;
+
     private static void Main(string[] args)
     {
         // Initialize GameInput Wrapper Object
         using var gameInput = GameInput.Create();
+        var hr = "=========================================";
 
-        while (true)
+        var keyboardContainers = new Dictionary<Guid, List<GameInputDevice>>();
+
+        // Enumerate through devices to group them by Container GUID
+        foreach (var device in gameInput.EnumerateDevices(
+                     inputKind))
         {
-            Thread.Sleep(1000);
-            try
-            {
-                var index = 1;
-                foreach (var input in
-                         gameInput.EnumerateDevices(GameInputKind.Controller))
-                {
-                    if (input == null)
-                    {
-                        Console.WriteLine("No keyboard input");
-                        Thread.Sleep(1000);
-                        continue;
-                    }
+            var info = device.GetDeviceInfo();
+            if (!keyboardContainers.ContainsKey(info.ContainerId))
+                keyboardContainers.Add(info.ContainerId,
+                    new List<GameInputDevice>());
 
-                    var info = input.GetDeviceInfo();
+            keyboardContainers[info.ContainerId].Add(device);
+        }
 
-                    Console.WriteLine(
-                        $"{index}:{info.ProductId}/{info.GetPnpPath()}: {info.SupportedInput}");
-                    index++;
-                }
-            }
-            catch (Exception ex)
+        PrintSection(keyboardContainers, inputKind);
+        Console.WriteLine();
+    }
+
+    private static void PrintSection(
+        Dictionary<Guid, List<GameInputDevice>> devices,
+        GameInputKind kind)
+    {
+        Console.WriteLine(
+            $"{ThickHr}\n" +
+            $" {kind.ToString().ToUpper()}\n" +
+            $"{ThickHr}");
+
+        foreach (var containerId in devices.Keys)
+        {
+            Console.Write(
+                $"{ThickHr}\n Container: {containerId}\n{ThickHr}\n");
+
+            foreach (var device in devices[containerId])
             {
-                var errorCode = (GameInputErrorCode)ex.HResult;
-                Console.WriteLine(ex.Message);
+                var info = device.GetDeviceInfo();
+
+                var displayName = "";
+                if (UsbIds.TryGetVendorName(info.VendorId, out var vendorName))
+                    displayName = $"{vendorName} ";
+
+                if (UsbIds.TryGetProductName(info.VendorId, info.ProductId,
+                        out var productName))
+                    displayName += $"{productName}";
+                else
+                    displayName += info.GetDisplayName();
+
+                Console.Write(
+                    $"ID: {info.DeviceId}\n" +
+                    $"DisplayName: {displayName}\n");
+                Console.Write($"{ThinHr}\n");
             }
+
+            Console.WriteLine(ThickHr + "\n");
         }
     }
 }
