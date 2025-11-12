fba444b0bd3a4b124bef8ac3d0290630cde5455c
Victoria Beauray Sagady <cephypi@pm.me>
2025-11-11 18:28:17 -0600

    fix: Improved Example

--- Examples/SimplePolling/Program.cs
+++ Examples/SimplePolling/Program.cs
@@ -12,7 +12,8 @@ gameInput.SetFocusPolicy(GameInputFocusPolicy.EnableBackgroundInput);
 while (true)
 {
     // Slow down the loop so we don't get super spammed.  This may result in
-    // missing fast keypresses.
+    // missing fast keypresses.  Use GetNextReading and track previous reading to
+    // avoid missing any input changes.
     Thread.Sleep(100);
 
     // Create reading object
@@ -22,7 +23,7 @@ while (true)
         // Get the current reading from the GameInput system. This allows us
         // to inquire at any time about the status of inputs.  You could also
         // use GetLastReading or GetNextReading.
-        reading = gameInput.GetCurrentReading(GameInputKind.Keyboard);
+        reading = gameInput.GetCurrentReading(GameInputKind.Keyboard | GameInputKind.Mouse);
     }
     catch (GameInputException)
     {
@@ -32,7 +33,10 @@ while (true)
     }
 
     // If we got a null value we don't want to throw a null pointer exception.
-    if (reading is null) continue;
+    if (reading is null)
+    {
+        continue;
+    }
 
     // This using ensures we properly dispose of the GameInputReading object
     // which implements IDisposable.
@@ -42,15 +46,41 @@ while (true)
     {
         // Get the current state. 
         var state = reading.GetKeyboardState();
+        var mouseState = reading.GetMouseState();
+
+        // Write every mouse button that is pressed.
+        if (mouseState != null)
+        {
+            if (mouseState.Buttons > 0)
+            {
+                foreach (GameInputMouseButtons button in Enum.GetValues(typeof(GameInputMouseButtons)))
+                {
+                    if (button == 0)
+                    {
+                        continue;
+                    }
+
+                    if (mouseState.Buttons.HasFlag(button))
+                    {
+                        Console.WriteLine($"{button.ToString()} pressed...");
+                    }
+                }
+            }
+        }
 
         if (state.Keys.Count == 0)
+        {
             continue;
+        }
 
         // Report each key that is currently pressed.
         Console.Write("Keys Pressed: ");
         // Print the name of the keys based on the virtual key byte.
         foreach (var key in state.Keys)
+        {
             Console.Write($"{(ConsoleKey)key.VirtualKey} ");
+        }
+
         Console.WriteLine();
 
         // We can also use this to end our program when the user hits Escape.
